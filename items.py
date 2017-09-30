9ad041b17218d256d9bbf48e7dc3597f74bcecc1
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 20:06:13 +0200

    proper formatting everywhere

--- items.py
+++ items.py
@@ -1,36 +1,36 @@
 files = {
     '/etc/yum.repos.d/jenkins.repo': {
-        'source': "jenkins.repo",
-        'mode': "0644",
+        'source': 'jenkins.repo',
+        'mode': '0644',
         'triggers': [
-            "action:dnf_makecache",
+            'action:dnf_makecache',
         ],
     },
     '/etc/sysconfig/jenkins': {
-        'source': "sysconfig",
-        'content_type': "mako",
-        'mode': "0600",
+        'source': 'sysconfig',
+        'content_type': 'mako',
+        'mode': '0600',
         'needs': [
-            "pkg_dnf:jenkins",
+            'pkg_dnf:jenkins',
         ],
         'triggers': [
-            "svc_systemd:jenkins:restart",
+            'svc_systemd:jenkins:restart',
         ],
     },
 }
 
 actions = {
     'jenkins_import_gpg_key': {
-        'command': "rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-        'unless': "rpm -ql gpg-pubkey-d50582e6-4a3feef6",
+        'command': 'rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key',
+        'unless': 'rpm -ql gpg-pubkey-d50582e6-4a3feef6',
     },
 }
 
 pkg_dnf = {
-    "jenkins": {
+    'jenkins': {
         'needs': [
-            "file:/etc/yum.repos.d/jenkins.repo",
-            "action:jenkins_import_gpg_key"
+            'file:/etc/yum.repos.d/jenkins.repo',
+            'action:jenkins_import_gpg_key'
         ],
     },
 }
@@ -38,17 +38,17 @@ pkg_dnf = {
 svc_systemd = {
     'jenkins': {
         'needs': [
-            "pkg_dnf:jenkins",
+            'pkg_dnf:jenkins',
         ],
     },
 }
 
-if node.has_bundle("monit"):
+if node.has_bundle('monit'):
     files['/etc/monit.d/jenkins'] = {
-        'source': "monit",
-        'mode': "0640",
-        'content_type': "mako",
+        'source': 'monit',
+        'mode': '0640',
+        'content_type': 'mako',
         'triggers': [
-            "svc_systemd:monit:restart",
+            'svc_systemd:monit:restart',
         ],
     }
